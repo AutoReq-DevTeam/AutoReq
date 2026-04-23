1f4d0a575f701a430fcf10778768bd8afd5966a7
Muhammed Eren Eyyüpkoca <mereneyyupkoca@gmail.com>
2026-04-23 09:59:01 +0300

    feat(modules): RequirementImprover, improver_prompts ve mock testler (Issue #14)

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,11 +5,21 @@ from .analysis_report_parsing import build_analysis_report_from_llm
 from .conflict_detector import ConflictDetector, PairwiseConflictAnalysis
 from .gap_analyzer import GapAnalyzer
 from .improver import RequirementImprover
+from .improver_prompts import (
+    CORE_IMPROVER_PERSONA,
+    IMPROVEMENT_SYSTEM_PROMPT,
+    build_improvement_system_prompt,
+    build_improvement_user_prompt,
+)
 
 __all__ = [
     "build_analysis_report_from_llm",
+    "build_improvement_system_prompt",
+    "build_improvement_user_prompt",
+    "CORE_IMPROVER_PERSONA",
     "ConflictDetector",
-    "PairwiseConflictAnalysis",
     "GapAnalyzer",
+    "IMPROVEMENT_SYSTEM_PROMPT",
+    "PairwiseConflictAnalysis",
     "RequirementImprover",
 ]
